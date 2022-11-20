250372de4b0dea25087808a23cadd0bf7c176a59
Alexander Myltsev <a.myltsev@gmail.com>
2022-11-21 00:05:59 +0300

    Fix trap frame layout.

--- cpu/isr.h
+++ cpu/isr.h
@@ -28,8 +28,8 @@ enum {
  * - `push eax` whose lower 16-bits contain DS
  */
 typedef struct {
-    uint32_t ds; /* Data segment selector */
     uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; /* Pushed by pusha. */
+    uint32_t gs, fs, es, ds; /* Data segment selector */
     uint32_t int_no, err_code; /* Interrupt number and error code (if applicable) */
     uint32_t eip, cs, eflags, useresp, ss; /* Pushed by the processor automatically */
 } registers_t;
