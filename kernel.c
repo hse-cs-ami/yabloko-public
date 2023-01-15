927c3055d19575c9d16280ed70e36e2e8991a2e6
Alexander Myltsev <a.myltsev@gmail.com>
2023-01-15 18:46:18 +0400

    Tester for vga-symbols.

--- kernel.c
+++ kernel.c
@@ -1,12 +1,21 @@
 #include "console.h"
 #include "drivers/vga.h"
 #include "drivers/uart.h"
+#include "drivers/misc.h"
 
+void show_vga_symbol_table(void);
 
 void _start() {
     uartinit();
 
-    vga_clear_screen();
-    printk("\nYABLOKO\n");
-    asm("hlt");
+    show_vga_symbol_table();
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            unsigned offset = i * COLS + j;
+            char c = video_memory[2 * offset];
+            uartputc(c);
+        }
+        uartputc('\n');
+    }
+    qemu_shutdown();
 }
