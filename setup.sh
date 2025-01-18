4fcdb1cdd8bec12f6c770cab7510c329ced3bc0a
Alexander Myltsev <a.myltsev@gmail.com>
2025-01-18 16:01:27 +0400

    Install lld on macOS.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 if [ `uname` = Darwin ]; then
     brew install \
         x86_64-elf-binutils x86_64-elf-gcc \
-        x86_64-elf-gdb qemu
+        x86_64-elf-gdb qemu lld
 elif [ `uname` = Linux ]; then
     sudo apt-get update
     sudo apt-get install qemu-system-x86
